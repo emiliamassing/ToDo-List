2d79bff5b76dc6918dd75b838b1eed4cf8b8f654
emiliamassing <emiliamassings@gmail.com>
2022-12-16 15:00:21 +0100

    fixed delete function,added date when task was added

--- src/main.ts
+++ src/main.ts
@@ -19,23 +19,25 @@ function printTaskList(): void {
   incompleteTasks.innerHTML = '';
   let toDoListHtml = '';
 
-  toDoList.forEach((task) => {
+  toDoList.forEach((task, index) => {
     toDoListHtml += `
     <li>
-      <input type="checkbox" class="checkCompleted">
+      <input type="checkbox" class="checkCompleted" tabindex="0">
       <label>
-        <input type="text" value="${task.taskName}" readonly>
+        <input type="text" value="${task.taskName}" id="editInput" readonly>
       </label>
+      <span class"deadline">${task.deadline}</span>
       <i></i>
-      <i class="fa-solid fa-pen-to-square fa-lg" tabindex="0"></i>
-      <i class="fa-solid fa-x fa-lg" tabindex="0"></i>
+      <i class="editIcon fa-solid fa-pen-to-square fa-lg" tabindex="0"></i>
+      <i class="removeIcon fa-solid fa-x fa-lg" tabindex="0" data-id="${index}"></i>
     </li> `;
   });
 
   incompleteTasks.innerHTML = toDoListHtml;
 
-  const tasks = Array.from(document.querySelectorAll('li i.fa-x'));
+  const tasks = Array.from(document.querySelectorAll('li i.removeIcon'));
   tasks.forEach((task) => {
+    console.log(tasks);
     task.addEventListener('click', removeTask); // eslint-disable-line
   });
 }
@@ -43,21 +45,24 @@ function printTaskList(): void {
 // Funktion för att ta bort todo
 function removeTask(e: Event) {
   const target = e.target as HTMLElement;
-  const index = toDoList.findIndex((task) => task.taskName === target.dataset.name);
-  if (index > -1) {
+  const index = Number(target.dataset.id);
+  console.log(index);
+  if (index !== undefined) {
     toDoList.splice(index, 1);
     printTaskList();
   }
 }
-
 printTaskList();
 
 const newTaskName = <HTMLInputElement>document.querySelector('#writeItem');
-
+const newDeadline = <HTMLInputElement>document.querySelector('#date');
+// newTaskName.value.length === 0
 // Funktion för att lägga till ny todo
-function addNewTask():void {
-  if (newTaskName.value.length === 0) {
-    alert('You need to write something');
+function addNewTask(e: Event):void {
+  e.preventDefault();
+  if (newTaskName.value.length === 0 || newDeadline.value.length === 0) {
+    // eslint-disable-next-line no-alert
+    alert('You need to write something and pick a date');
     return;
   }
 
@@ -65,15 +70,19 @@ function addNewTask():void {
   if (index === -1) {
     // TODO: Fyll på med rätt data för kategori, deadline, dateAdded
     // pusha in som en "instans av din ToDoItem-klass" istället
-    toDoList?.push(new ToDoItem(newTaskName.value, 'household', 'deadline', 'dateAdded', false));
+    const dateAdded = new Date();
+    toDoList?.push(new ToDoItem(newTaskName.value, 'household', 'deadline', dateAdded, false));
     printTaskList();
+    console.log(dateAdded);
   } else {
+    // eslint-disable-next-line no-alert
     alert("You've already added this task");
   }
 }
 
 const addTaskBtn = document.querySelector('#addItemBtn');
 addTaskBtn?.addEventListener('click', addNewTask);
+
 // I denna utils-fil har vi lagrat funktioner som ofta används, t.ex. en "blanda array"-funktion
 // import { shuffle } from './utils';
 

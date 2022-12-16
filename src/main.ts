8a604445b0f692909fd55cc876e8d1d7c7c2c192
emiliamassing <emiliamassings@gmail.com>
2022-12-16 11:44:19 +0100

    changes structure of print tasklist

--- src/main.ts
+++ src/main.ts
@@ -14,53 +14,25 @@ darkModeBtn?.addEventListener('click', toggleDarkMode);
 
 const incompleteTasks: HTMLElement = document.querySelector('#incompleteTasks') as HTMLElement;
 
-// Kod för att skriva ut li-elementen - Be om hjälp med detta, ifall det är bättre att göra såhär?
-// Ska även innehålla deadline + vald kategori
-const toDoListHtml = `
-  <li>
-    <input type="checkbox" class="checkCompleted">
-    <i class="fa-solid fa-pen-to-square fa-lg" tabindex="0"></i>
-    <i class="fa-solid fa-x fa-lg" tabindex="0"></i>
-  </li>
-`;
-
 // Funktion för att skriva ut en ny todo
 function printTaskList(): void {
   incompleteTasks.innerHTML = '';
+  let toDoListHtml = '';
 
-  for (let i = 0; i < toDoList.length; i++) {
-    const taskName = toDoList[i].taskName;
-    const taskNode = document.createElement('li');
-    const taskTextNode = document.createTextNode(taskName);
+  toDoList.forEach((task) => {
+    toDoListHtml += `
+    <li>
+      <input type="checkbox" class="checkCompleted">
+      <label>
+        <input type="text" value="${task.taskName}" readonly>
+      </label>
+      <i></i>
+      <i class="fa-solid fa-pen-to-square fa-lg" tabindex="0"></i>
+      <i class="fa-solid fa-x fa-lg" tabindex="0"></i>
+    </li> `;
+  });
 
-    // const editIcon = document.createElement('button');
-    // const editIconTextNode = document.createTextNode('edit');
-    // editIcon.classList.add('i');
-    const editIcon = document.createElement('i');
-    editIcon.classList.add('fa-solid');
-    editIcon.classList.add('fa-pen-to-square');
-    editIcon.classList.add('fa-lg');
-
-    editIcon.setAttribute('tabindex', '0'); // 0 gör så att browsern själv avgör i vilken ordning element ska tabbas igenom, bäst att låta det vara så utifrån a11y
-
-    /* const removeIcon = document.createElement('button');
-    const removeIconTextNode = document.createTextNode('Delete');
-    removeIcon.setAttribute('data-name', taskName);
-    removeIcon.appendChild(removeIconTextNode); */
-
-    // Dubbelkolla att nedanstående text är okej innan du raderar de övre blockkomentarerna
-
-    const removeIcon = document.createElement('i');
-    removeIcon.classList.add('fa-solid');
-    removeIcon.classList.add('fa-x');
-    removeIcon.classList.add('fa-lg');
-    removeIcon.setAttribute('data-name', taskName);
-
-    taskNode.appendChild(taskTextNode);
-    taskNode.appendChild(editIcon);
-    taskNode.appendChild(removeIcon);
-    incompleteTasks?.appendChild(taskNode);
-  }
+  incompleteTasks.innerHTML = toDoListHtml;
 
   const tasks = Array.from(document.querySelectorAll('li i.fa-x'));
   tasks.forEach((task) => {
@@ -71,8 +43,7 @@ function printTaskList(): void {
 // Funktion för att ta bort todo
 function removeTask(e: Event) {
   const target = e.target as HTMLElement;
-  const index = toDoList.findIndex(task => task.taskName === target.dataset.name); // Då du nu har objekt i din array, så måste vi leta efter dessa på ett annat sätt
-  // const index = toDoList.indexOf(target.dataset.name);
+  const index = toDoList.findIndex((task) => task.taskName === target.dataset.name);
   if (index > -1) {
     toDoList.splice(index, 1);
     printTaskList();
@@ -90,13 +61,13 @@ function addNewTask():void {
     return;
   }
 
-  const index = toDoList.findIndex(task => task.taskName === newTaskName.value); // Då du nu har objekt i din array, så måste vi leta efter dessa på ett annat sätt
-
+  const index = toDoList.findIndex((task) => task.taskName === newTaskName.value);
   if (index === -1) {
     // TODO: Fyll på med rätt data för kategori, deadline, dateAdded
-    toDoList?.push(new ToDoItem(newTaskName.value, 'household', 'deadline', 'dateAdded', false)); // pusha in som en "instans av din ToDoItem-klass" istället
+    // pusha in som en "instans av din ToDoItem-klass" istället
+    toDoList?.push(new ToDoItem(newTaskName.value, 'household', 'deadline', 'dateAdded', false));
     printTaskList();
-  } else { // Räcker med en else bara, behövs ingen else if :)
+  } else {
     alert("You've already added this task");
   }
 }

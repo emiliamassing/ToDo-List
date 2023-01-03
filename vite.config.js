52b240d01d04e123baa2247c78cb25e0940defe6
emiliamassing <emiliamassings@gmail.com>
2023-01-03 12:56:38 +0100

    changed base to correct repo

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ då ska "base" här nedan vara "/fed22d-js-grundkurs-jenni-wumpus/"
  */
 
 export default defineConfig({
-  base: '/js-template/', // TODO - ändra till ditt repo-namn
+  base: '/ToDo-List/',
   define: {
     'process.env': {},
   },

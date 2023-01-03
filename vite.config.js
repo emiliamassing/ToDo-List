c7cdb23068eb8e5577ba2c2a06bf645033f48107
emiliamassing <emiliamassings@gmail.com>
2023-01-03 18:00:12 +0100

    removed comment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite';
 
-/*
-Om ditt repo har adressen https://github.com/Medieinstitutet/fed22d-js-grundkurs-jenni-wumpus,
-då ska "base" här nedan vara "/fed22d-js-grundkurs-jenni-wumpus/"
- */
-
 export default defineConfig({
   base: '/ToDo-List/',
   define: {
